c8cea4c97901fa63071f169c899b51d35af52589
Gil <caperkid11122@gmail.com>
2024-04-29 19:58:54 -0400

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -7,16 +7,22 @@ from service.common import status
 
 COUNTER = {}
 
+
+############################################################
 # Health Endpoint
+############################################################
 @app.route("/health")
 def health():
     """Health Status"""
     return jsonify(dict(status="OK")), status.HTTP_200_OK
 
+
+############################################################
 # Index page
+############################################################
 @app.route("/")
 def index():
-    """Returns information about the service"""
+    """Returns information abut the service"""
     app.logger.info("Request for Base URL")
     return jsonify(
         status=status.HTTP_200_OK,
@@ -25,22 +31,33 @@ def index():
         url=url_for("list_counters", _external=True),
     )
 
+
+############################################################
 # List counters
+############################################################
 @app.route("/counters", methods=["GET"])
 def list_counters():
     """Lists all counters"""
     app.logger.info("Request to list all counters...")
+
     counters = [dict(name=count[0], counter=count[1]) for count in COUNTER.items()]
+
     return jsonify(counters)
 
+
+############################################################
 # Create counters
+############################################################
 @app.route("/counters/<name>", methods=["POST"])
 def create_counters(name):
     """Creates a new counter"""
-    app.logger.info(f"Request to Create counter: {name}...")
+    app.logger.info("Request to Create counter: %s...", name)
+
     if name in COUNTER:
         return abort(status.HTTP_409_CONFLICT, f"Counter {name} already exists")
+
     COUNTER[name] = 0
+
     location_url = url_for("read_counters", name=name, _external=True)
     return (
         jsonify(name=name, counter=0),
@@ -48,37 +65,56 @@ def create_counters(name):
         {"Location": location_url},
     )
 
+
+############################################################
 # Read counters
+############################################################
 @app.route("/counters/<name>", methods=["GET"])
 def read_counters(name):
     """Reads a single counter"""
-    app.logger.info(f"Request to Read counter: {name}...")
+    app.logger.info("Request to Read counter: %s...", name)
+
     if name not in COUNTER:
         return abort(status.HTTP_404_NOT_FOUND, f"Counter {name} does not exist")
+
     counter = COUNTER[name]
     return jsonify(name=name, counter=counter)
 
+
+############################################################
 # Update counters
+############################################################
 @app.route("/counters/<name>", methods=["PUT"])
 def update_counters(name):
     """Updates a counter"""
-    app.logger.info(f"Request to Update counter: {name}...")
+    app.logger.info("Request to Update counter: %s...", name)
+
     if name not in COUNTER:
         return abort(status.HTTP_404_NOT_FOUND, f"Counter {name} does not exist")
+
     COUNTER[name] += 1
+
     counter = COUNTER[name]
     return jsonify(name=name, counter=counter)
 
+
+############################################################
 # Delete counters
+############################################################
 @app.route("/counters/<name>", methods=["DELETE"])
 def delete_counters(name):
     """Deletes a counter"""
-    app.logger.info(f"Request to Delete counter: {name}...")
+    app.logger.info("Request to Delete counter: %s...", name)
+
     if name in COUNTER:
         COUNTER.pop(name)
+
     return "", status.HTTP_204_NO_CONTENT
 
+
+############################################################
 # Utility for testing
+############################################################
 def reset_counters():
     """Removes all counters while testing"""
     global COUNTER  # pylint: disable=global-statement
